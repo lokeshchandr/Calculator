c704625e17dc08eb1a69b846bf3a1ec774b8eba4
lokesh <lokeshdongre93@gmail.com>
2024-11-24 12:24:22 +0530

    debug 2

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input) {
+  if(display.value === "ERROR")
+  {
+    display.value =" ";
+  }
   display.value += input;
 }
 
 function calculate() {
   try {
     display.value = eval(display.value); //evel: calculate the value
-  } catch {
+  } catch(error) {
     display.value = "ERROR";
+  
   }
 }
 function clearDisplay() {
